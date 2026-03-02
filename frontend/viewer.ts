8715a206ddaee5739115b190b73ef57d07b37707
unknown <melodyhalftone@hotmail.com>
2026-03-01 22:40:17 -0500

    more null check

--- frontend/viewer.ts
+++ frontend/viewer.ts
@@ -208,7 +208,7 @@ async function run(config: common.MapshotConfig, info: common.MapshotJSON, crash
 	
 	var num_days = 0;
 	
-	if (config !== undefined && config != null) {
+	if (config !== undefined && config != null && config["encoded_path"] != null) {
 		parseInt(config["encoded_path"].match(/\d+/)[0]);
 	}
 	days.push({label: "Day 0", value: "d-0"});
