047dfcf1317cbb6eb060fa53262d6ff0767f365d
unknown <melodyhalftone@hotmail.com>
2026-03-06 21:55:11 -0500

    d6

--- frontend/viewer.ts
+++ frontend/viewer.ts
@@ -143,7 +143,7 @@ class Surface {
             this.combatHeatLayer = (L as any).heatLayer(points, {
                 radius: 64,
                 blur: 64,
-                maxZoom: 4,
+                maxZoom: 5,
                 gradient: {
                     0.0: '#0000ff',
                     0.3: '#00ffff',
